a273e1ca3d3e7cb37e22757c248229a18a2a67e5
Kei Jay <keijayk@gmail.com>
2022-01-09 22:42:14 +0900

    formated terraform code
    Signed-off-by: Kei Jay <keijayk@gmail.com>

--- terraform/util.tf
+++ terraform/util.tf
@@ -9,7 +9,9 @@ resource "null_resource" "updatedb_container_create" {
       --subnet ${azurerm_subnet.container_instance_subnet.name} \
       --vnet ${azurerm_virtual_network.virtual_network.name} \
       --restart-policy Never \
-      --environment-variables "VTT_DBPASSWORD"="${azurerm_key_vault_secret.postgres_key_vault_secret.value}" "VTT_DBUSER"="${var.postgresql_db_user}"  "VTT_DBUSERWITHHOST"="${var.postgresql_db_user_with_host}"  "VTT_DBHOST"="${var.postgresql_db_host}"  "VTT_DBLISTENHOST"="${var.postgresql_db_listen_host}" \
+      --environment-variables "VTT_DBPASSWORD"="${azurerm_key_vault_secret.postgres_key_vault_secret.value}"  \
+      "VTT_DBUSER"="${var.postgresql_db_user}"  "VTT_DBUSERWITHHOST"="${var.postgresql_db_user_with_host}"   \
+      "VTT_DBHOST"="${var.postgresql_db_host}"  "VTT_DBLISTENHOST"="${var.postgresql_db_listen_host}" \
       --registry-username ${azurerm_container_registry.container_registry.admin_username} \
       --registry-password ${azurerm_container_registry.container_registry.admin_password}
       EOT
@@ -22,7 +24,8 @@ resource "null_resource" "updatedb_container_create" {
 resource "null_resource" "updatedb_container_remove" {
       provisioner "local-exec" {
       command = <<-EOT
-      ../scripts/check_status.sh "${azurerm_resource_group.resource_group.name}" "${var.container_name}" "${var.expected_log_message}"
+      ../scripts/check_status.sh "${azurerm_resource_group.resource_group.name}" "${var.container_name}"  \
+      "${var.expected_log_message}"
       az container delete --yes --resource-group ${azurerm_resource_group.resource_group.name} \
       --name ${var.container_name}
       EOT
@@ -36,7 +39,9 @@ resource "null_resource" "docker_push" {
       provisioner "local-exec" {
       command = <<-EOT
         docker tag ${var.app_container_image_name_tag} ${local.app_container_image}
-        docker login --username ${azurerm_container_registry.container_registry.admin_username} --password ${azurerm_container_registry.container_registry.admin_password} ${azurerm_container_registry.container_registry.login_server}
+        docker login --username ${azurerm_container_registry.container_registry.admin_username}  \
+        --password ${azurerm_container_registry.container_registry.admin_password}  \
+        ${azurerm_container_registry.container_registry.login_server}
         docker push ${local.app_container_image}
       EOT
       }
